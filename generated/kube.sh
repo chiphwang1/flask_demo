8a845cf34d80eef31ac589d1636b3d1d7e2e0aca
ociobenablement/identitycloudservice/chiping.hwang@oracle.com <test@example.com>
2022-06-13 11:12:27 -0700

    change tf

--- generated/kube.sh
+++ generated/kube.sh
@@ -1,1 +1,3 @@
-kubectl get nodes
+#!/bin/bash
+
+kubectl -n example get --kubeconfig /tmp/kubeconfig svc server-lb-service -o jsonpath='{.status.loadBalancer.ingress[0]}'
---
+++ kube.tf
@@ -0,0 +1,43 @@
+provider "kubernetes" {
+  kubernetes {
+    config_path = "/tmp/kubeconfig"
+  }
+}
+
+
+
+resource "time_sleep" "wait_for_kube" {
+  depends_on = [
+  local_file.kubeconfig
+  ]
+  create_duration = "60s"
+}
+
+resource "kubernetes_namespace" "demo3" {
+  depends_on = [
+  local_file.kubeconfig
+  ]
+  metadata {
+    name = "demo-namespace"
+  }
+}
+
+
+
+resource "null_resource" "example1" {
+   depends_on = [time_sleep.wait_1min]
+  provisioner "local-exec" {
+    command = "/bin/bash ./generated/kube.sh > /tmp/ip_addr" 
+    
+  }
+}
+
+data "local_file" "ip_addr" {
+    depends_on = [null_resource.example1]
+    filename = "/tmp/ip_addr"
+}
+
+output "ip_endpoint" {
+  value       = "Endpoint is ${data.local_file.ip_addr.content} "
+  description = "Endpoint for website"
+}
