027b5755cccd99009d4e3325763540dd92580fed
ociobenablement/identitycloudservice/chiping.hwang@oracle.com <test@example.com>
2022-06-13 13:41:53 -0700

    v18

--- generated/kube.sh
+++ generated/kube.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl -n example get --kubeconfig /tmp/kubeconfig svc server-lb-service -o jsonpath='{.status.loadBalancer.ingress[0]}'
+kubectl -n demo3 get --kubeconfig /tmp/kubeconfig svc server-lb-service -o jsonpath='{.status.loadBalancer.ingress[0]}'
